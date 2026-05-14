8559ac494be64f38c664499ab774970bfe30dac9
Subash <sapkotasubash784@gmail.com>
2026-05-14 23:09:17 +0200

    ss

---
+++ S_5/index.js
@@ -0,0 +1,50 @@
+console.log("Day 5")
+//The for loop 
+//Syntax:
+//for (initialization;condition;update){  code }
+
+
+for (let count=1; count<=5; count++){console.log("Iteration/loop", count)}
+
+//Addition of even numbers between 1 to 100
+let sum=0;
+for (let i=1; i<=100; i++){
+      if(i%2===0){
+            sum+=i;
+
+      }
+}
+console.log("The sum is ", sum);
+
+
+
+
+
+let language="JavaScript";
+
+for (let i=0; i<language.length;i++){
+      console.log(language.charAt(i));
+
+}
+
+
+//Nested loop 
+
+for(let i=1; i<=3; i++){
+      for (let j=1; j<=3; j++){
+            console.log("Row", i, "col", j)
+      }
+}
+
+
+//Break and Continue 
+for(let i=1; i<=5; i++){
+      if (i===3)break;
+      console.log(i)
+
+}
+//continue 
+for (let i=1; i<=5; i++) {
+    if (i===3) continue;
+    console.log(i)
+}
