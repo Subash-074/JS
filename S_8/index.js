98236d23a7119ead684a834cf9510906c0d6483a
Subash <sapkotasubash784@gmail.com>
2026-05-24 13:51:44 +0200

    ss

---
+++ S_8/index.js
@@ -0,0 +1,18 @@
+var name = 'Tom';
+
+function sayName(){
+      console.log(this.name);
+}
+
+sayName();
+
+
+var name='Tom';
+
+
+function tom(){
+      console.log(this.name+'Runs');
+
+}
+//Invoke the function tom()
+
