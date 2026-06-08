698d9ae35d67bccb3ff84d47a2654f12eecb6121
Subash <sapkotasubash784@gmail.com>
2026-06-08 15:44:08 +0200

    ss

---
+++ S_12/index.js
@@ -0,0 +1,24 @@
+/*
+
+Javascript Objects
+There will be situation in programming where you have to handle complex data that is when you need to know something beyond just the primitive type like boolean structure.
+Objects are nothing but the key value pair. It can be identified using key value pair.
+Objects can be created in multiple ways. 
+
+
+*/
+
+let user={
+      name:"Subash",
+      age:21,
+
+}
+//Accessing Objects 
+console.log(user.name);
+
+//once this particular has been created can we add more properties to this object.
+
+user.isSeniorCitizen=false;
+
+console.log(user);
+ 
